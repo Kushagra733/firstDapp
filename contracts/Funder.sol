fe0e85266eff98bc8d650c21a9f464992dd02953
Kushagra733 <kushagramishra733@gmail.com>
2022-05-27 13:07:08 +0530

    add files

---
+++ contracts/Funder.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract Funder{
+    uint public numberOfFunders;
+
+    mapping(uint=>address) private funders;
+
+    receive() external payable{}
+    
+    function transfer() external payable{
+        funders[numberOfFunders]=msg.sender;
+    }
+
+    function withdraw(uint withdrawAmount)  external{
+        require(withdrawAmount <= 2000000000000000000, "cannot withdraw");
+        payable(msg.sender).transfer(withdrawAmount);
+    }
+
+
+}
